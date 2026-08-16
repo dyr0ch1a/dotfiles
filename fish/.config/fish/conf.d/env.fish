ca061dae8e941058b1d491109756ffb6e0a069b3
dyr0ch1a <nik.chernia@gmail.com>
2026-08-16 19:44:55 +0500

    #swicth: Screenshoots to Wl-Copy and alt file to save

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -1,8 +1,9 @@
-set -gx OLLAMA_MODELS /home/dyr0ch1a/Disk/Disk_A/LLM/
+set -gx OLLAMA_MODELS /home/dyr0ch1a/d/LLM/
 set -gx OLLAMA_API_BASE http://localhost:11434
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin $PATH /home/dyr0ch1a/.ghcup/bin # ghcup-env
 set -g UV_LINK_MODE symlink
 set -g XDG_CONFIG_HOME "/home/dyr0ch1a/.config/"
+set -g MISE_DATA_DIR $HOME/d/Programs/Data/mise
 set -g _ZO_FZF_OPTS
 set -gx TERMINAL kitty
